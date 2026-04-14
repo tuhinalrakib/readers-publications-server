5be7682028e328e0ac45b0857a228c174ee86335
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-04-14 12:32:09 +0600

    whitenoise

--- config/settings/production.py
+++ config/settings/production.py
@@ -22,3 +22,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
